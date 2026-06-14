0522f15c7028e2800d302372ac3dbfbc76e135fd
Tod Famous <tod@famousfamily.com>
2026-06-13 20:45:29 -0400

    Allow saving progress while game is off

--- app/save-progress/page.tsx
+++ app/save-progress/page.tsx
@@ -1,16 +1,8 @@
-import { GameUnavailablePage } from "@/components/GameUnavailablePage";
 import { SaveProgressForm } from "@/components/SaveProgressForm";
-import { getGameAvailability } from "@/lib/db";
 
 export const dynamic = "force-dynamic";
 export const runtime = "nodejs";
 
 export default async function SaveProgressPage() {
-  const availability = await getGameAvailability();
-
-  if (!availability.enabled) {
-    return <GameUnavailablePage />;
-  }
-
   return <SaveProgressForm />;
 }
